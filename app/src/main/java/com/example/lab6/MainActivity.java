4f6fcd22312ec4d775ee97564a480598224da234
alegom05 <146905500+alegom05@users.noreply.github.com>
2025-06-12 21:10:39 -0500

    push

---
+++ app/src/main/java/com/example/lab6/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.lab6;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null) {
+            launchSignIn();
+        } else {
+            Log.d("msg-test", "Usuario ya autenticado: " + currentUser.getUid());
+        }
+    }
+
+    private void launchSignIn() {
+        Intent intent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(Arrays.asList(
+                        new AuthUI.IdpConfig.EmailBuilder().build(),
+                        new AuthUI.IdpConfig.GoogleBuilder().build()
+                ))
+                .build();
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Log.d("msg-test", "Firebase uid: " + user.getUid());
+
+                // Opcional: ir a HomeActivity o pantalla principal
+                // startActivity(new Intent(this, HomeActivity.class));
+                // finish();
+
+            } else {
+                if (response == null) {
+                    Log.d("msg-test", "Login cancelado por el usuario");
+                    finish(); // o qué hacer en ese caso
+                } else {
+                    Log.e("msg-test", "Error en login", response.getError());
+                }
+            }
+        }
+    }
+}
